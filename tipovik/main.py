fb6b0ed195e0a92fa829a098af0b62cdd6054cfe
NixGot <m-mol.molodtsov@yandex.ru>
2024-11-17 23:28:32 +0300

    end work

--- tipovik/main.py
+++ tipovik/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
 
 
@@ -9,9 +10,10 @@ def find_n0(f, e, a, n=1, step=1):
         else:
             n += step
 
-def task4_6(f, e, lim, xlim1=1, xlim2=101, ylim1=0, ylim2=0.6):
+def task4_6(f, e, lim, title,xlim1=1, xlim2=101, ylim1=0, ylim2=0.6):
     plt.figure(figsize=(12, 6))
     y = find_n0(f, e, lim, step=2)
+    plt.title(title)
     plt.ylim(ylim1, ylim2)
     plt.scatter([n for n in range(xlim1, y, 2)], [f(n) for n in range(xlim1, y, 2)], color='red', s=8)
     plt.scatter([n for n in range(y, xlim2, 2)], [f(n) for n in range(y, xlim2, 2)], color='green', s=8)
@@ -19,11 +21,15 @@ def task4_6(f, e, lim, xlim1=1, xlim2=101, ylim1=0, ylim2=0.6):
     plt.plot([xlim1-1, xlim2+1], [lim, lim], color='pink')
     plt.plot([xlim1-1, xlim2+1], [lim+e, lim+e], color='cyan', alpha=0.3)
     plt.plot([xlim1-1, xlim2+1], [lim-e, lim-e], color='cyan', alpha=0.3)
+    plt.legend(['точки, которые не входят в $\\varepsilon$-окрестность', 'точки, которые входят в $\\varepsilon$-окрестность','точки, которые не входят в подпоследовательность' , 'предел подпоследовательности' , '$\\varepsilon$-окрестность'], loc='lower right')
+    plt.ylabel('$x_n$')
+    plt.xlabel('$n$')
     plt.show()
 
-def task7_9(f, e, lim, xlim1=2, xlim2=101, ylim1=0, ylim2=0.6):
+def task7_9(f, e, lim, title, xlim1=2, xlim2=101, ylim1=0, ylim2=0.65):
     plt.figure(figsize=(12, 6))
     y = find_n0(f, e, lim, n=2, step=2)
+    plt.title(title)
     plt.ylim(ylim1, ylim2)
     plt.scatter([n for n in range(xlim1, xlim2, 2)], [f(n) for n in range(xlim1, xlim2, 2)], color='black', s=8)
     plt.scatter(y, f(y), color='green', s=8)
@@ -31,11 +37,13 @@ def task7_9(f, e, lim, xlim1=2, xlim2=101, ylim1=0, ylim2=0.6):
     plt.plot([xlim1-1, xlim2+1], [lim+e, lim+e], color='cyan', alpha=0.3)
     plt.plot([xlim1-1, xlim2+1], [lim-e, lim-e], color='cyan', alpha=0.3)
     print(f'для окрестности {e}, x_n={y}')
+    plt.legend(['точки $x_n$', 'точка, которая входит в $\\varepsilon$-окрестность', '$\sup x_n$' , '$\\varepsilon$-окрестность'], loc='lower right')
+    plt.ylabel('$x_n$')
+    plt.xlabel('$n$')
     plt.show()
 
 
 plt.figure(figsize=(12, 6))
-
 f = lambda n: np.arctan(np.sqrt(2 + (-1)**n))*n/(2*n+5)
 
 plt.scatter([n for n in range(1, 101, 2)], [f(n) for n in range(1, 101, 2)], color='red', s=8)
@@ -45,15 +53,19 @@ plt.plot([0, 100], [np.pi/6, np.pi/6]) #sup
 plt.plot([0, 100], [np.pi/28, np.pi/28]) #inf
 plt.plot([0, 100], [np.pi/6, np.pi/6]) #uplim
 plt.plot([0, 100], [np.pi/8, np.pi/8]) #unlim
+plt.legend(['$x_{2n}$', '$x_{2n+1}$', '$\sup \ x_n$', '$\inf \ x_n$', 'верхний предел', 'нижний предел'], loc='lower right')
+plt.ylabel('$x_n$')
+plt.xlabel('$n$')
+plt.title('Задание 2-3')
 plt.show()
 
 
-task4_6(f, 0.1, np.pi/8)
-task4_6(f, 0.05, np.pi/8)
-task4_6(f, 0.001, np.pi/8, xlim1=941, xlim2=1020, ylim1=0.390, ylim2=0.395)
-task4_6(f, 0.0001, np.pi/8, xlim1=9801, xlim2=9900, ylim1=0.3925, ylim2=0.3929)
+task4_6(f, 0.1, np.pi/8, 'Задание 4-6 для $\\varepsilon=0.1$')
+task4_6(f, 0.05, np.pi/8, 'Задание 4-6 для $\\varepsilon=0.05$')
+task4_6(f, 0.001, np.pi/8, 'Задание 4-6 для $\\varepsilon=0.001$', xlim1=941, xlim2=1020, ylim1=0.390, ylim2=0.395)
+task4_6(f, 0.0001, np.pi/8, 'Задание 4-6 для $\\varepsilon=0.0001$', xlim1=9801, xlim2=9900, ylim1=0.3924, ylim2=0.3929)
 
-task7_9(f, 0.1, np.pi/6)
-task7_9(f, 0.05, np.pi/6)
-task7_9(f, 0.001, np.pi/6, xlim1=1280, xlim2=1350, ylim1=0.5223, ylim2=0.5250)
-task7_9(f, 0.0001, np.pi/6, xlim1=13050, xlim2=13120, ylim1=0.5233, ylim2=0.5238)
+task7_9(f, 0.1, np.pi/6, 'Задание 7-9 для $\\varepsilon=0.1$')
+task7_9(f, 0.05, np.pi/6, 'Задание 7-9 для $\\varepsilon=0.05$')
+task7_9(f, 0.001, np.pi/6, 'Задание 7-9 для $\\varepsilon=0.001$', xlim1=1280, xlim2=1350, ylim1=0.5217, ylim2=0.5255)
+task7_9(f, 0.0001, np.pi/6, 'Задание 7-9 для $\\varepsilon=0.0001$', xlim1=13050, xlim2=13120, ylim1=0.5233, ylim2=0.5238)
